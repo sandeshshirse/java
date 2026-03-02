e870af6a3488306b4f1dbb20ec1a9dde6fc4c2d8
Sandesh Shirse <shirse170906@gmail.com>
2026-03-02 21:30:51 +0530

    Create practice class with basic data type examples
    Added a Java class to demonstrate basic data types.

---
+++ javaBasic/dataType.java
@@ -0,0 +1,21 @@
+public class practice {
+    public static void main(String[] args) {
+        
+        String name = "Sandesh Shirse";
+        System.out.println(name);
+
+        int age = 20;
+        System.out.println("my age is " + age);
+
+        
+        float marks = 20.20f;                            // for using foalt in java, we have to end the num with 'f' 
+        System.out.println("my marks are " + marks);
+
+        char grade = 'S';
+        System.out.println("my grade is " + grade);
+
+        boolean gender = true;
+        System.out.println(gender);
+
+    }
+}
