f21e79e7d79b04838c04cab01e8d1db80a6c63e0
Sandesh Shirse <shirse170906@gmail.com>
2026-03-02 21:17:17 +0530

    Add Main class with greeting messages

---
+++ practice/Main.java
@@ -0,0 +1,7 @@
+class Main {
+    public static void main(String[] args){
+        System.out.println("Hello world!!");
+        System.out.println("I am Sandesh Shirse");
+        System.out.println("Rahul is my son, he is very naughty");
+    }
+}
