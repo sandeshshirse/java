c1f07a2181c46702b2573e57a7f1fb2c03c67930
Sandesh Shirse <shirse170906@gmail.com>
2026-03-05 23:11:04 +0530

    Add StudentProfile class with main method

---
+++ javaBasic/studentProfile.java
@@ -0,0 +1,14 @@
+public class StudentProfile{
+    public static void main(String[] args) {
+        String name = "Sandesh Shirse";
+        int age = 19;
+        double CGPA = 8.7;
+        char division = 'F';
+        boolean isDefaulter = false;
+
+        System.out.println("Name " + name);
+        System.out.println("Age " + age);
+        System.out.println("CGPA " + CGPA);
+        System.out.println("Div " + division);
+    }
+}
