1560d0a786eb98facc0e1af6f4cf83cc4292104e
Sandesh Shirse <shirse170906@gmail.com>
2026-03-06 22:09:58 +0530

    Add string methods example in strings.java
    Implement string methods demonstration in Java.

---
+++ javaBasic/strings.java
@@ -0,0 +1,35 @@
+//string meathods
+
+
+
+public class strings{
+    public static void main (String[] args) {
+        
+        //string length
+
+        String name = "sandesh shirse";
+        System.out.println("The length of the strings is : " + name.length());
+
+
+        //uppercaselowercase
+
+        System.out.println("uppercase : " + name.toUpperCase());
+
+        //lowercase
+        System.out.println("uppercase : " + name.toLowerCase());
+
+        //indexOf
+        System.out.println(name.indexOf("shirse"));
+
+        System.out.println(name.indexOf("shi"));
+
+        
+
+
+    
+
+
+
+    }
+
+}
