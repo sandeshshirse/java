6736cd27781007d7d3366c307395732284558e66
Sandesh Shirse <shirse170906@gmail.com>
2026-03-10 23:13:58 +0530

    Add function to calculate square of a number

---
+++ javaBasic/functions.java
@@ -0,0 +1,12 @@
+public class function {
+    public static int square(int num){
+    return num*num;
+}
+
+public static void main(String[] args) {
+
+   
+    System.out.println("Square is : " + square(4));
+
+}    
+}
